a09a8c97c61cead67af0994733f602c2e9a3acbe
William <harv.wc@gmail.com>
2019-08-13 11:34:46 -0400

    added description

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ namespace Find_Duplicate
 {
     class Program
     {
+        // Given an array of length (n+1) whose members belong to the set {1,2,3,...,n}. By the pigeonhole principle,
+        // there must be a duplicate. Return the duplicate value.
         static void Main(string[] args)
         {
             int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 17 };
